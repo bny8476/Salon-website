cf4fd192ed0cad2eda719c7f378dc6f402fbef86
Naveen Kumar <naveenkumarv.16523@gmail.com>
2026-08-24 14:59:57 +0530

    Temporarily add Flyway repair strategy

--- backend/src/main/java/com/luxesuite/api/config/ApplicationConfig.java
+++ backend/src/main/java/com/luxesuite/api/config/ApplicationConfig.java
@@ -7,10 +7,19 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 
 @Configuration
 @EnableScheduling
 public class ApplicationConfig {
+
+    @Bean
+    public FlywayMigrationStrategy repairMigrateStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
 
     @Bean
     public AuthenticationManager authenticationManager(
